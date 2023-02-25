056c5a69563edc17b96cecb1e846613c2c75d278
Carl Nysten <nysten.carl@gmail.com>
2023-02-25 23:07:27 +0200

    Added linear_regression.h

---
+++ lib_linear_reg/includes/linear_regression.h
@@ -0,0 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   linear_regression.h                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cnysten <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/25 22:59:16 by cnysten           #+#    #+#             */
+/*   Updated: 2023/02/25 23:00:17 by cnysten          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef  LINEAR_REGRESSION_H
+# define LINEAR_REGRESSION_H
+
+void	train(int x_vals[], int x_len, int y_vals[], int y_len);
+
+#endif

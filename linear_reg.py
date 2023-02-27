018c60732b5e6ca176130df764fa76629f5d5937
Carl Nysten <nysten.carl@gmail.com>
2023-02-28 00:07:45 +0200

    Turned GradientDescentResult into C_LinearModel, replaced generic with number union

--- linear_reg.py
+++ linear_reg.py
@@ -1,27 +1,32 @@
 from ctypes import CDLL, POINTER, c_int, c_float, Structure
-from typing import List, TypeVar
+from typing import List, Union
 
-
-class GradientDescentResult(Structure):
+class C_LinearModel(Structure):
+    # Add missing fields? Or make py_fit() return just w and b and reuse the x_values and y_values
     _fields_ = [('w', c_float),
                 ('b', c_float)]
 
 lib_linear_reg = CDLL("lib_linear_reg.so")
-lib_linear_reg.train.restype = POINTER(GradientDescentResult)
+lib_linear_reg.fit.restype = POINTER(C_LinearModel)
+lib_linear_reg.predict.restype = c_float
 
+# Make this into dataclass?
 class LinearRegressionModel:
-    T = TypeVar("T")
+    T = Union[int, float]
     
     def train(self, x_values: List[T], y_values: List[T]) -> None:
         XArray = c_int * len(x_values)
         YArray = c_int * len(y_values)
-        result = lib_linear_reg.train(XArray(*x_values), len(x_values), YArray(*y_values), len(y_values))
+        model = lib_linear_reg.fit(XArray(*x_values), len(x_values), YArray(*y_values), len(y_values))
         print(x_values)
         print(y_values)
-        print("result in python -> w:", result.contents.w, "b:", result.contents.b)
+        print("result in python -> w:", model.contents.w, "b:", model.contents.b)
+        # Free result
+        # Convert c model to LinearRegressionModel
+        # Return self?
         pass
 
-    def predict(self, val: T) -> T: 
+    def predict(self, val: T) -> float:
         prediction = lib_linear_reg.predict(val)
         print(f"Prediction: {prediction}")
         return prediction
